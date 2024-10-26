caad1c1ba9102737950aee7f2bd17c9db18d3eb6
shoaib <sohebmohammadd@gmail.com>
2024-10-27 00:38:50 +0530

    slide brands

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,10 +1,10 @@
-import brands from "./brands.png";
 import b1 from "./b1.png";
 import b2 from "./b2.png";
 import b3 from "./b3.png";
 import b4 from "./b4.png";
 import b5 from "./b5.png";
 import b6 from "./b6.png";
+import "./Brands.css"; // Assuming you are storing CSS in Brands.css
 
 function Brands() {
   return (
@@ -14,22 +14,22 @@ function Brands() {
         style={{ backgroundColor: "#f4d84f" }}
       >
         <div className="col-1">
-          <img className="w-100" src={b1} alt="Brand 1"></img>
+          <img className="w-100 brand-img" src={b1} alt="Brand 1"></img>
         </div>
         <div className="col-1">
-          <img className="w-100" src={b2} alt="Brand 2"></img>
+          <img className="w-100 brand-img" src={b2} alt="Brand 2"></img>
         </div>
         <div className="col-1">
-          <img className="w-100" src={b3} alt="Brand 3"></img>
+          <img className="w-100 brand-img" src={b3} alt="Brand 3"></img>
         </div>
         <div className="col-1">
-          <img className="w-100" src={b4} alt="Brand 4"></img>
+          <img className="w-100 brand-img" src={b4} alt="Brand 4"></img>
         </div>
         <div className="col-1">
-          <img className="w-100" src={b5} alt="Brand 5"></img>
+          <img className="w-100 brand-img" src={b5} alt="Brand 5"></img>
         </div>
         <div className="col-1">
-          <img className="w-100" src={b6} alt="Brand 6"></img>
+          <img className="w-100 brand-img" src={b6} alt="Brand 6"></img>
         </div>
       </div>
     </>
