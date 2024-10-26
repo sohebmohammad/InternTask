b00890fc454cc04c3e0357fd51db70aff3f0789e
shoaib <sohebmohammadd@gmail.com>
2024-10-27 00:12:12 +0530

    Voucher changes

--- src/components/Vouchers.js
+++ src/components/Vouchers.js
@@ -1,9 +1,39 @@
-import v from "./Vouchers.png";
+import v1 from "./v1.png";
+import m from "./Rectangle 18.png";
+import m2 from "./Rectangle 55.png";
+import mobile from "./Mobile app.png";
+
 function Vouchers() {
   return (
     <>
       <div className="container-fluid">
-        <img className="col-12" src={v}></img>
+        <div className="row d-flex justify-content-center align-items-center">
+          {/* Left Column */}
+          <div className="col-lg-6 col-md-12 d-flex flex-column justify-content-center align-items-start gap-4 p-4">
+            <div>
+              <img src={v1} alt="Voucher Banner" className="img-fluid" />
+            </div>
+            <div>
+              <p className="text-body fs-5">
+                Get 30% off for first transaction using Rondovision mobile app
+                for now.
+              </p>
+            </div>
+            <div className="d-flex gap-3">
+              <button className="btn btn-outline-dark">
+                <img src={m} alt="App Store" className="img-fluid" />
+              </button>
+              <button className="btn btn-outline-dark">
+                <img src={m2} alt="Play Store" className="img-fluid" />
+              </button>
+            </div>
+          </div>
+
+          {/* Right Column */}
+          <div className="col-lg-6 col-md-12 d-flex justify-content-center justify-content-lg-end p-4">
+            <img src={mobile} alt="Mobile App" className="img-fluid" />
+          </div>
+        </div>
       </div>
     </>
   );
